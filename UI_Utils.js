77bc57c58deaaf0c9932611b6629e2e3da24d146
Bubba Skrimp <brewster.joshua1@gmail.com>
2021-06-04 13:44:39 -0700

    fixes

--- UI_Utils.js
+++ UI_Utils.js
@@ -3,11 +3,15 @@
 //Example:
 let events = new ObjectListener();
 let x = { y: 1, z: { w: 2 }}
+
+
 events.addListener("y",x,"y");
 events.addListener("z",x,"z");
+
 x.z.w = 3;
 x.y = 2;
 //See console
+
 */
 
 //By Joshua Brewster (MIT)
@@ -210,7 +214,8 @@ export class ObjectListenerInstance {
 
     //Execute extra onchange functions
     onchangeMulti = (newData) => {
-        this.onchangeFuncs.forEach((func,i) => {
+        let onChangeCache = [...this.onchangeFuncs]
+        onChangeCache.forEach((func,i) => {
             if(this.debug === true) { console.log(func); }
             func(newData);
         });
@@ -259,7 +264,9 @@ export class ObjectListenerInstance {
             if(this.propOld !== string){
                 if(this.debug === true) { console.log("onchange: ", this.onchange); }
                 this.onchange(this.object[this.propName]);
-                if(this.onchangeFuncs.length > 0) { this.onchangeMulti(this.object[this.propName]); }
+                if(this.onchangeFuncs.length > 0) { 
+                    this.onchangeMulti(this.object[this.propName]); 
+                }
                 this.setListenerRef(this.propName);
             }
         }
@@ -381,84 +388,6 @@ export function sortObjectByPropName(object) {
     return sorted;
 
 }
-
-//modified to also cut down the size arrays for faster looping
-if(JSON.stringifyFast === undefined) {
-    //Workaround for objects containing DOM nodes, which can't be stringified with JSON. From: https://stackoverflow.com/questions/4816099/chrome-sendrequest-error-typeerror-converting-circular-structure-to-json
-    JSON.stringifyFast = (function() {
-        const refs = new Map();
-        const parents = [];
-        const path = ["this"];
-
-        function clear() {
-            refs.clear();
-            parents.length = 0;
-            path.length = 1;
-        }
-
-        function updateParents(key, value) { //for json.parse
-            var idx = parents.length - 1;
-            var prev = parents[idx];
-            if (prev[key] === value || idx === 0) {
-                path.push(key);
-                parents.push(value);
-            } else {
-                while (idx-- >= 0) {
-                    prev = parents[idx];
-                    if (prev[key] === value) {
-                        idx += 2;
-                        parents.length = idx;
-                        path.length = idx;
-                        --idx;
-                        parents[idx] = value;
-                        path[idx] = key;
-                        break;
-                    }
-                }
-            }
-        }
-
-        function checkValues(key, value) {
-            let val = value;
-            if (val !== null) {
-                if (typeof value === "object") {
-                    //if (key) { updateParents(key, value); }
-                    let other = refs.get(val);
-                    let c = value.constructor.name;
-                    if (other) {
-                        return '[Circular Reference]' + other;
-                    } else if(c === "Array" && value.length > 20) { //Cut arrays down to 100 samples for referencing
-                        val = value.slice(value.length-20);
-                        refs.set(val, path.join('.'));
-                    } else if (c !== "Object" && c !== "Number" && c !== "String" && c !== "Boolean") { //simplify classes, objects, and functions, point to nested objects for the state manager to monitor those properly
-                        val = "instanceof_"+c;
-                        refs.set(val, path.join('.'));
-                    } else if (typeof val === 'object') {
-                        let obj = {};
-                        for(const prop in val) {
-                            if(Array.isArray(val[prop])) { obj[prop] = val[prop].slice(val[prop].length-20); } //deal with arrays in nested objects (e.g. means, slices)
-                            else { obj[prop] = val[prop]; }
-                        }
-                    }
-                    else {
-                        refs.set(val, path.join('.'));
-                    }
-                }
-            }
-            return val;
-        }
-
-        return function stringifyFast(obj, space) {
-            try {
-                parents.push(obj);
-                return JSON.stringify(obj, checkValues, space);
-            } finally {
-                clear();
-            }
-        }
-    })();
-}
-
 
 //By Joshua Brewster (MIT)
 
@@ -640,6 +569,7 @@ export class DOMFragment {
 }
 
 
+
 //By Joshua Brewster (MIT)
 //Simple state manager.
 //Set key responses to have functions fire when keyed values change
@@ -649,11 +579,20 @@ export class StateManager {
         this.data = init;
         this.interval = interval;
         this.pushToState={};
-        this.prev = Object.assign({},this.data);;
-                
+       
+        // Allow Updates to State to Be Subscribed To
+        this.update = {added:'', removed: '', buffer: new Set()}
+        this.updateCallbacks = {
+                added: [],
+                removed: []
+        }
+
+
         this.listener = new ObjectListener();
 
         /*
+        this.prev = Object.assign({},this.data);
+         
         const onStateChanged = () => {
             this.prev = Object.assign({},this.data);
             //this.prev=JSON.parse(JSON.stringifyFast(this.data));
@@ -669,6 +608,8 @@ export class StateManager {
         );
         */
 
+
+
     }
 
     setInterval(interval="FRAMERATE") {
@@ -678,14 +619,36 @@ export class StateManager {
         });
     }
 
-    //Alternatively just add to the state by doing this.state[key] = value with the state manager instance
-    addToState(key, value, onchange=null, debug=false) {
-        if(!this.listener.hasKey('push')) {
+
+    // Managed State Updates. Must Still Clean Event Listeners
+    updateState(key, value){
+        if (this.data[key] == null){
+            this.addToState(key,value)
+        } else {
+            this.data[key] = value
+        }    
+    }
+
+    removeState(key){
+            this.unsubscribeAll(key);
+            delete this.data[key]
+
+            // Log Update
+            this.update.removed = key
+            this.update.buffer.add( key )
+    }
+
+    setupSynchronousUpdates() {
+        if(!this.listener.hasKey('pushToState')) {
+
+            this.addToState('update',this.update, this.onUpdate)
+
             //we won't add this listener unless we use this function
             const pushToStateResponse = () => {
                 if(Object.keys(this.pushToState).length > 0) {
-                    Object.assign(this.prev,this.data);//Temp fix until the global state listener function works as expected
+                    //Object.assign(this.prev,this.data);//Temp fix until the global state listener function works as expected
                     Object.assign(this.data,this.pushToState);
+
                     //console.log("new state: ", this.data); console.log("props set: ", this.pushToState);
                     for (const prop of Object.getOwnPropertyNames(this.pushToState)) {
                         delete this.pushToState[prop];
@@ -694,15 +657,27 @@ export class StateManager {
             }
     
             this.listener.addListener(
-                "push",
+                "pushToState",
                 this.pushToState,
                 "__ANY__",
                 pushToStateResponse,
                 this.interval
             );
-    
         }
+    }
+
+    //Alternatively just add to the state by doing this.state[key] = value with the state manager instance
+    addToState(key, value, onchange=null, debug=false) {
+        if(!this.listener.hasKey('pushToState')) {
+            this.setupSynchronousUpdates();
+        }
+
         this.data[key] = value;
+
+        // Log Update
+        this.update.added = key
+        this.update.buffer.add( key )
+
         if(onchange !== null){
             return this.addSecondaryKeyResponse(key,onchange,debug);
         }
@@ -712,8 +687,47 @@ export class StateManager {
         return JSON.parse(JSON.stringifyFast(this.data));
     }
 
+    //Synchronous set-state, only updates main state on interval.
     setState(updateObj={}){ //Pass object with keys in. Undefined keys in state will be added automatically. State only notifies of change based on update interval
         //console.log("setting state");
+        if(!this.listener.hasKey('pushToState')) {
+            this.setupSynchronousUpdates();
+        }
+        
+        for(const prop in updateObj) { //3 object-deep array checks to buffer values instead of overwriting
+            if(this.pushToState.hasKey(prop)) {
+                if(Array.isArray(this.pushToState[prop]) && Array.isArray(updateObj[prop])) {
+                    updateObj[prop] = this.pushToState[prop].push(...updateObj[prop]);
+                } else if (typeof this.pushToState[prop] === 'object' && typeof updateObj[prop] === 'object') {
+                    for(const p in updateObj[prop]) {
+                        if(this.pushToState[prop].hasKey(p)) {
+                            if(Array.isArray(this.pushToState[prop][p]) && Array.isArray(updateObj[prop][p])) {
+                                updateObj[prop][p] = this.pushToState[prop][p].push(...updateObj[prop][p]);
+                            }
+                            else if (typeof this.pushToState[prop][p] === 'object' && typeof updateObj[prop][p] === 'object') {
+                                for(const p2 in updateObj[prop][p]) {
+                                    if(this.pushToState[prop][p].hasKey(p2)) {
+                                        if(Array.isArray(this.pushToState[prop][p][p2]) && Array.isArray(updateObj[prop][p][p2])) {
+                                            updateObj[prop][p][p2] = this.pushToState[prop][p][p2].push(...updateObj[prop][p][p2]);
+                                        }
+                                    }
+                                    else if (typeof this.pushToState[prop][p][p2] === 'object' && typeof updateObj[prop][p][p2] === 'object') {
+                                        for(const p3 in updateObj[prop][p][p2]) {
+                                            if(this.pushToState[prop][p][p2].hasKey(p3)) {
+                                                if(Array.isArray(this.pushToState[prop][p][p2][p3]) && Array.isArray(updateObj[prop][p][p2][p3])) {
+                                                    updateObj[prop][p][p2][p3] = this.pushToState[prop][p][p2][p3].push(...updateObj[prop][p][p2][p3]);
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
         Object.assign(this.pushToState,updateObj);
         return this.pushToState;
     }
@@ -749,6 +763,8 @@ export class StateManager {
         if(key !== null) {
             if(this.listener.hasKey(key)){
                 this.listener.removeFuncs(key, responseIdx);
+            } else {
+                console.error("key does not exist")
             }
         }
         else{console.error("provide key")}
@@ -756,13 +772,12 @@ export class StateManager {
 
     //Remove any extra object listeners for a key. Entering "state" will break the state manager's primary response
     clearAllKeyResponses(key=null) {
-        if(this.listener.hasKey(key))
-            this.listener.remove(key);
+        if(this.listener.hasKey(key)) this.listener.remove(key);
     }
 
     //Save the return value to provide as the responseIdx in unsubscribe
     subscribe(key, onchange) {
-        if(this.data[key] === undefined) {this.addToState(k,null,onchange);}
+        if(this.data[key] === undefined) {this.addToState(key,null,onchange);}
         else {return this.addSecondaryKeyResponse(key,onchange);}
     }
 
@@ -776,4 +791,115 @@ export class StateManager {
         this.clearAllKeyResponses(key);
     }
 
+    addUpdateFunction = (added,removed) => {
+        if (added) this.updateCallbacks.added.push(added)
+        if (removed) this.updateCallbacks.removed.push(removed)
+    }
+
+    onUpdate = (update) => {
+
+        update.buffer.delete('update')
+
+        if (update.added){
+            this.updateCallbacks.added.forEach(f => {
+                if (f instanceof Function) f(update.buffer)
+            })
+        }
+
+        if (update.removed){
+            this.updateCallbacks.removed.forEach(f => {
+                if (f instanceof Function) f(update.buffer)
+            })
+        }
+
+        update.added = ''
+        update.removed = ''
+        update.buffer.clear()
+    }
+
+}
+
+
+//modified to also cut down the size arrays for faster looping
+if(JSON.stringifyFast === undefined) {
+    //Workaround for objects containing DOM nodes, which can't be stringified with JSON. From: https://stackoverflow.com/questions/4816099/chrome-sendrequest-error-typeerror-converting-circular-structure-to-json
+    JSON.stringifyFast = (function() {
+        const refs = new Map();
+        const parents = [];
+        const path = ["this"];
+
+        function clear() {
+            refs.clear();
+            parents.length = 0;
+            path.length = 1;
+        }
+
+        function updateParents(key, value) { //for json.parse
+            var idx = parents.length - 1;
+            var prev = parents[idx];
+            if (prev[key] === value || idx === 0) {
+                path.push(key);
+                parents.push(value);
+            } else {
+                while (idx-- >= 0) {
+                    prev = parents[idx];
+                    if (prev[key] === value) {
+                        idx += 2;
+                        parents.length = idx;
+                        path.length = idx;
+                        --idx;
+                        parents[idx] = value;
+                        path[idx] = key;
+                        break;
+                    }
+                }
+            }
+        }
+
+        function checkValues(key, value) {
+            let val = value;
+            if (val !== null) {
+                if (typeof value === "object") {
+                    //if (key) { updateParents(key, value); }
+                    let other = refs.get(val);
+                    let c = value.constructor.name;
+                    if (other) {
+                        return '[Circular Reference]' + other;
+                    } else if(c === "Array" && value.length > 20) { //Cut arrays down to 100 samples for referencing
+                        val = value.slice(value.length-20);
+                        refs.set(val, path.join('.'));
+                    } else if (c !== "Object" && c !== "Number" && c !== "String" && c !== "Boolean") { //simplify classes, objects, and functions, point to nested objects for the state manager to monitor those properly
+                        val = "instanceof_"+c;
+                        refs.set(val, path.join('.'));
+                    } else if (typeof val === 'object') {
+                        let obj = {};
+                        for(const prop in val) {
+                            if(Array.isArray(val[prop])) { obj[prop] = val[prop].slice(val[prop].length-20); } //deal with arrays in nested objects (e.g. means, slices)
+                            else if (typeof val[prop] === 'object') { //additional layer of recursion for 3 object-deep array checks
+                                obj[prop] = {};
+                                for(const p in val[prop]) {
+                                    if(Array.isArray(val[prop][p])) { obj[prop][p] = val[prop][p].slice(val[prop][p].length-20); }
+                                    else { obj[prop][p] = val[prop][p]; }
+                                }
+                            }
+                            else { obj[prop] = val[prop]; }
+                        }
+                    }
+                    else {
+                        refs.set(val, path.join('.'));
+                    }
+                }
+            }
+            return val;
+        }
+
+        return function stringifyFast(obj, space) {
+            try {
+                parents.push(obj);
+                return JSON.stringify(obj, checkValues, space);
+            } finally {
+                clear();
+            }
+        }
+    })();
 }
